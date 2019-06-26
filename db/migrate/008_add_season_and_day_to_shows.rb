a7ec49d7190e957f75bbd27e781e293166dc79d0
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-26 22:04:48 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_season_and_day_to_shows.rb
+++ db/migrate/008_add_season_and_day_to_shows.rb
@@ -2,6 +2,5 @@ class AddSeasonAndDayToShows < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :season, :string
     add_column :shows, :day, :string
-    add_column :shows, :genre, :string
   end
 end
